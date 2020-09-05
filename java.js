8fa93872dfea627e9dfb3fde5fe1aaa7a10d986e
altair0606 <47645928+altair0606@users.noreply.github.com>
2020-09-05 19:32:19 +0200

    saigo

--- java.js
+++ java.js
@@ -1,12 +1,5 @@
 // AOS.init({ disable: 'mobile' });
-// $(function(){ 
-//   var navMain = $(".navbar-collapse"); // avoid dependency on #id
-//   // "a:not([data-toggle])" - to avoid issues caused
-//   // when you have dropdown inside navbar
-//   navMain.on("click", "a:not([data-toggle])", null, function () {
-//       navMain.collapse('hide');
-//   });
-// });
+
 
 $('.navbar-nav>li>a').on('click', function(){
   $('.navbar-collapse').collapse('hide');
